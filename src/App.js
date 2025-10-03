6b18475f15d7c28343d6ab54c51245dda89cd9d1
Srinivasa Manikanta <srinivasmanikantarajapantula@gmail.com>
2025-10-03 09:49:57 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        {/* Navigation bar can be added here later */}
+      </header>
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
