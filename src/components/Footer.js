6e376c3626a5806d38e0577d2d67474a07dfe1ef
Srinivasa Manikanta <srinivasmanikantarajapantula@gmail.com>
2025-10-03 09:49:04 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ padding: '2rem 0', borderTop: '1px solid #333', marginTop: '2rem' }}>
+      <p>&copy; 2025 RSMK. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
